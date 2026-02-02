082ea447dbaa3587f03b27c3de2b1eed9e46bd17
saksham <sakshamgarg056@gmail.com>
2026-02-02 10:27:31 +0530

    project structure setup properly

---
+++ CanteenAutomationApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using CanteenBackend.Models;
+
+namespace CanteenBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ using CanteenBackend.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
